f3ebe3d4790817e96e115f8922794ba9b279eef5
Hammas-Shahid <143096092+Hammas-Shahid@users.noreply.github.com>
2024-08-03 21:59:27 +0500

    Passwords temporarily removed

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -6,12 +6,20 @@ import { Injectable } from '@angular/core';
 export class AuthService {
   private verifiedRoutes: Set<string> = new Set();
 
+  // private pins: { [key: string]: string } = {
+  //   'metric-dimension': 'qwerty9211',
+  //   'vector-calculator': 'kholo2',
+  //   'information-system': 'asdf420',
+  //   'direct-distance': 'kholo2',
+  //   'comparison-analysis': 'asdf9211'
+  // };
+
   private pins: { [key: string]: string } = {
-    'metric-dimension': 'qwerty9211',
-    'vector-calculator': 'kholo2',
-    'information-system': 'asdf420',
-    'direct-distance': 'kholo2',
-    'comparison-analysis': 'asdf9211'
+    'metric-dimension': ' ',
+    'vector-calculator': ' ',
+    'information-system': ' ',
+    'direct-distance': ' ',
+    'comparison-analysis': ' '
   };
 
   isRouteVerified(route: string): boolean {
